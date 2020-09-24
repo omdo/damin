44de33580862e21eb0f6436da39eb1f0256860cb
omdo <onodnawij@gmail.com>
2020-09-24 18:20:47 +0700

    add backlayer

--- mods/list.py
+++ mods/list.py
@@ -1,8 +1,10 @@
+from kivy.uix.boxlayout import BoxLayout
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.list import OneLineIconListItem, ILeftBodyTouch
 from kivymd.uix.behaviors import RectangularElevationBehavior, TouchBehavior
+from kivymd.theming import ThemableBehavior
 from kivy.lang import Builder
-from kivy.properties import ObjectProperty, NumericProperty, BooleanProperty
+from kivy.properties import ObjectProperty, NumericProperty, BooleanProperty, StringProperty
 from kivy.core.window import Window
 
 Builder.load_string('''
@@ -13,12 +15,37 @@ Builder.load_string('''
 		MDFlatButton
 			_no_ripple_effect: True
 			size_hint: 1, 1
-			text: str(root.index)
+			text: str(root.num)
 			font_size: '19sp'
+			opacity: 1 if root.with_icon else 0
 		MDSeparator:
 			orientation: 'vertical'
 			color: .7, .7, .7, .1
 			width: dp(4)
+			opacity: 1 if root.with_icon else 0
+
+<SelectableListItem>
+    size_hint_y: None
+    height: self.minimum_height
+    spacing: "10dp"
+
+    canvas:
+        Color:
+            rgba:
+                root.theme_cls.primary_dark if root.selected_item \
+                else root.theme_cls.primary_color
+        RoundedRectangle:
+            pos: self.pos
+            size: self.size
+
+    MDIconButton:
+        icon: root.icon
+        theme_text_color: "Custom"
+        text_color: (0, 0, 0, .5) if not root.selected_item else (0, 0, 0, 1)
+
+    MDLabel:
+        text: root.text
+        color: (0, 0, 0, .5) if not root.selected_item else (0, 0, 0, 1)
 ''')
 
 class Lefty(MDBoxLayout, ILeftBodyTouch):
@@ -26,7 +53,27 @@ class Lefty(MDBoxLayout, ILeftBodyTouch):
 
 class CustomOneLineListItem(OneLineIconListItem, RectangularElevationBehavior, TouchBehavior):
 	callback = ObjectProperty()
-	index = NumericProperty()
+	num = NumericProperty()
+	with_icon = BooleanProperty(True)
 
 	def on_release(self):
 		self.callback(self)
+
+class SelectableListItem(ThemableBehavior, BoxLayout):
+    icon = StringProperty("android")
+    text = StringProperty()
+    selected_item = BooleanProperty(False)
+    callback = ObjectProperty(None, allownone=True)
+
+    def on_touch_down(self, touch):
+        if self.collide_point(touch.x, touch.y):
+            for item in self.parent.children:
+                if item.selected_item:
+                    item.selected_item = False
+            self.selected_item = True
+        return super().on_touch_down(touch)
+
+    def on_selected_item(self, instance, value):
+    	if self.callback:
+    		if value:
+    			self.callback(self)
--- screens/main.py
+++ screens/main.py
@@ -3,7 +3,7 @@ from kivy.lang import Builder
 from kivy.properties import ListProperty, StringProperty, ObjectProperty, NumericProperty
 from mods.backdrop import MDBackdropFrontLayer
 from kivy.uix.recycleview import RecycleView
-from mods.list import CustomOneLineListItem
+from mods.list import CustomOneLineListItem, SelectableListItem
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.list import OneLineIconListItem
 from kivymd.uix.button import MDFlatButton
@@ -15,16 +15,33 @@ Builder.load_string('''
 		id: backdrop
 		right_action_items: root.right_menu
 		header_text: root.current_template
+		MDBackdropBackLayer:
+			BackLayer:
+				id: back_layer
+				rw: root
 		MDBackdropFrontLayer:
 			md_bg_color: .5, .5, .5, 1
 			FrontLayer:
+				id: front_layer
 				rw: root
 
 <FrontLayer>:
 	viewclass: 'CustomOneLineListItem'
 	RecycleBoxLayout:
+		id: box
 		padding: 10
 		spacing: 8
+		default_size: None, dp(56)
+        default_size_hint: 1, None
+        size_hint_y: None
+        height: self.minimum_height
+        orientation: 'vertical'
+
+<BackLayer>:
+	viewclass: 'SelectableListItem'
+	RecycleBoxLayout:
+		id: box
+		padding: 10
 		default_size: None, dp(56)
         default_size_hint: 1, None
         size_hint_y: None
@@ -45,16 +62,30 @@ class FrontLayer(RecycleView):
 	
 	def on_rw(self, inst, value):
 		if value:
-			print(value)
 			self.data = [
 				{
 					'text': f'List Item {x+1}',
-					'index': x+1,
+					'num': x+1,
 					'callback': self.rw.front_callback,
 					'divider': None,
 					'bg_color': [1, 1, 1, 1]
 				}
 				for x in range(100)
+			]
+
+class BackLayer(RecycleView):
+	rw = ObjectProperty()
+
+	def on_rw(self, inst, value):
+		if value:
+			templates = ['Program', 'Tamu', 'Keuangan', 'Rapat', 'Kerja']
+			self.data = [
+				{
+					'text': f'Buku {templates[i]}',
+					'num': 1,
+					'callback': self.rw.back_callback,
+				}
+				for i in range(len(templates))
 			]
 
 class DialogOption(OneLineIconListItem):
@@ -80,8 +111,7 @@ class MainScreen(Screen):
 		elif btn.icon == self.right_menu[2][0]:
 			screen = self.manager.app.screens.about
 		else:
-			print('FIX ME!! #REFRESH')
-			return
+			return print('FIX ME!! #REFRESH')
 		self.manager.switch_to(screen)
 
 	def option_callback(self, btn, item):
@@ -93,10 +123,17 @@ class MainScreen(Screen):
 			self.open_remove_dialog(item)
 
 	def remove_callback(self, btn):
-		if btn.text == 'OK':
-			print(self.remove_dialog.item, 'deleted')
-			print('FIX ME!!')
 		self.remove_dialog.dismiss()
+		if btn.text == 'OK':
+			index = None
+			self.ids.front_layer.data = sorted(self.ids.front_layer.data, key=lambda k: k['num'])
+			for i in range(len(self.ids.front_layer.data)):
+				if self.remove_dialog.item.num == self.ids.front_layer.data[i]['num']:
+					index = i
+					break
+			if index is not None:
+				self.ids.front_layer.data.pop(index)
+				return
 		self.dialog.open()
 
 	def open_remove_dialog(self, item):
@@ -124,3 +161,6 @@ class MainScreen(Screen):
 				]
 			)
 		self.dialog.open()
+
+	def back_callback(self, item):
+		print(item.text, 'selected')
